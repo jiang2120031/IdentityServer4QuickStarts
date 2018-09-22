524870da2208b226b88218a4b016dfefb58100c3
ZJF <jiang212003123@gmail.com>
2018-09-22 20:13:37 +0800

    Structrue Folder for client

--- IdentityServerQuickStart/IdentityServerQuickStart/Startup.cs
+++ IdentityServerQuickStart/IdentityServerQuickStart/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace IdentityServerQuickStart
 {
@@ -22,6 +24,30 @@ namespace IdentityServerQuickStart
                 .AddInMemoryApiResources(Config.GetApiResources())
                 .AddInMemoryClients(Config.GetClients())
                 .AddTestUsers(Config.GetUsers());
+
+            services.AddAuthentication()
+            .AddGoogle("Google", options =>
+            {
+                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+
+                options.ClientId = "434483408261-55tc8n0cs4ff1fe21ea8df2o443v2iuc.apps.googleusercontent.com";
+                options.ClientSecret = "3gcoTrEDPPJ0ukn_aYYT6PWo";
+            })
+             .AddOpenIdConnect("oidc", "OpenID Connect", options =>
+             {
+                 options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                 options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+
+                 options.Authority = "https://demo.identityserver.io/";
+                 options.ClientId = "implicit";
+
+                 options.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     NameClaimType = "name",
+                     RoleClaimType = "role"
+                 };
+             });
+            ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
